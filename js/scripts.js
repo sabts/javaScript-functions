88a41ccb73f0a6438db2fe18bd32d4be0f77acbe
sabts <sabrinataraschi@gmail.com>
2025-03-17 23:20:48 +0100

    21

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,126 @@
-function exercise1(calls, hours) {
-  const Result ('Camilla atiende por hora' calls / hours)
-  console.log(calls hours);
+function exercise1(callsNumber, callHour) {
+  const result = callsNumber / callHour;
+  console.log ( 'Camila atiende ' + result + ' llamadas por hora');
 }
-calls(120)
-hours(4)
+exercise1(120, 4)
+
+function excercise2(pencilBoxs,pencils){
+  const result = pencilBoxs * pencils
+  console.log ( 'Bego tiene ' + result + ' lápices')
+}
+excercise2(5,12)
+
+function excercise3(candies,group){
+  const result = candies % group
+  console.log ( 'A Camila le sobrará ' + result + ' caramelo después del reparto')
+}
+excercise3(25,4)
+
+function excercise4(km,hour){
+  let kmHour = km / hour
+  const result = kmHour * 2
+  console.log ( 'Sabrina recorrerá ' + result + ' kilómetros en dos horas')
+}
+excercise4(90,3)
+
+function excercise5(apple,boxes){
+  const result = apple / boxes
+  console.log ( 'Cada caja tendrá ' + result + ' manzanas')
+}
+excercise5(240,8)
+
+function excercise6(screw,boxes){
+  const result = screw % boxes
+  console.log ( 'Quedará por fuera ' + result + ' tornillos')
+}
+excercise6(57,10)
+
+function excercise7(mission,euros){
+  const result = mission * euros
+  console.log ( 'Abby ganó ' + result + ' euros')
+}
+excercise7(5,35)
+
+function excercise8(cookies,boxes){
+  const result = cookies % boxes
+  console.log ( 'A sabrina le sobrarán ' + result + ' galletas')
+}
+excercise8(92,8)
+
+function excercise9(book){
+  const result = book + 23
+  console.log ( 'Camila tiene ahora ' + result + ' libros')
+}
+excercise9(45)
+
+function excercise10(coins, showcase){
+  const result = coins % showcase
+  console.log ( 'No cabrán ' + result + ' monedas')
+}
+excercise10(123,5)
+
+ function excercise11(gasoline, km){
+  const result = km * 42 / 7
+  console.log ( 'Bego podrá recorrer ' + result + 'km en coche')
+}
+excercise11(7,100)
+
+ function excercise12(chickens, chickenCoop){
+  const result = chickens * chickenCoop
+  console.log ( 'Sabrina tiene ' + result + ' gallinas')
+}
+excercise12(25,6)
+
+function excercise13(bullets, gunspace){
+  const result = bullets % gunspace
+  console.log ( 'A Abby le sobrarán ' + result + ' balas')
+}
+excercise13(48,7)
+
+function excercise14(morningRun, afternoonRun){
+  const result = morningRun + afternoonRun
+  console.log ( 'Bego corrió en total ' + result + ' km')
+}
+excercise14(3,4)
+
+function excercise15(paper, paperSheet){
+  const result = paper % paperSheet
+  console.log ( 'Van a quedar por fuera ' + result + ' hojas')
+}
+excercise15(100,6)
+
+function excercise16(euros, acounts){
+  const result = euros / acounts
+  console.log ( 'Cada cuenta recibirá ' + result + ' euros')
+}
+excercise16(30000,4)
+
+function excercise17(zombies, days){
+  const result = zombies / days
+  console.log ( 'Abby mata ' + result + ' infectados al día')
+}
+excercise17(5201,7)
+
+function excercise18(marbels, marbelBox){
+  const result = marbels * marbelBox
+  console.log ( 'Bego tiene en total ' + result + ' canicas')
+}
+excercise18(24,3)
+
+function excercise19(coins,begosCoin){
+  const result= coins - begosCoin
+  console.log ('El hermano de Bego puso ' + result + ' monedas')
+}
+excercise19(135,78)
+
+function excercise20(legoPieces,lot){
+  const result= legoPieces % lot
+  console.log ('Van a quedar por fuera ' + result + ' piezas de lego')
+}
+excercise20(500,80)
+
+function excercise21(cookies,dogs){
+  const result= cookies / dogs
+  console.log ('Cada perro recibirá ' + result + ' galletas')
+}
+excercise21(72,9)
