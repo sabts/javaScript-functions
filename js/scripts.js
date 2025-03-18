982eb761d0d3204bd01da0d1fbb4477730fb81eb
sabts <sabrinataraschi@gmail.com>
2025-03-18 11:16:12 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
+function sum (numberA, numberB) {
+  const result = numberA + numberB;
+  return result
+}
+
+function division (numberA, numberB) {
+  const result = numberA / numberB;
+  return result
+}
+
+function multiply (numberA, numberB){
+  const result = numberA * numberB;
+  return result
+}
+
+function remainderOfDivision (numberA, numberB) {
+  const result =numberA % numberB
+  return result
+}
+
+
 function exercise1(callsNumber, callHour) {
-  const result = callsNumber / callHour;
+  const result = division(callsNumber,callHour);
   console.log ( 'Camila atiende ' + result + ' llamadas por hora');
 }
 exercise1(120, 4)
 
 function excercise2(pencilBoxs,pencils){
-  const result = pencilBoxs * pencils
+  const result = multiply (pencilBoxs,pencils);
   console.log ( 'Bego tiene ' + result + ' lápices')
 }
 excercise2(5,12)
 
 function excercise3(candies,group){
-  const result = candies % group
+  const result = remainderOfDivision (candies,group);
   console.log ( 'A Camila le sobrará ' + result + ' caramelo después del reparto')
 }
 excercise3(25,4)
@@ -24,37 +45,37 @@ function excercise4(km,hour){
 excercise4(90,3)
 
 function excercise5(apple,boxes){
-  const result = apple / boxes
+  const result = division (apple, boxes);
   console.log ( 'Cada caja tendrá ' + result + ' manzanas')
 }
 excercise5(240,8)
 
 function excercise6(screw,boxes){
-  const result = screw % boxes
+  const result = remainderOfDivision(screw,boxes);
   console.log ( 'Quedará por fuera ' + result + ' tornillos')
 }
 excercise6(57,10)
 
 function excercise7(mission,euros){
-  const result = mission * euros
+  const result = multiply(mission,euros)
   console.log ( 'Abby ganó ' + result + ' euros')
 }
 excercise7(5,35)
 
 function excercise8(cookies,boxes){
-  const result = cookies % boxes
+  const result = remainderOfDivision (cookies,boxes);
   console.log ( 'A sabrina le sobrarán ' + result + ' galletas')
 }
 excercise8(92,8)
 
 function excercise9(book){
-  const result = book + 23
+  const result = sum(book,23)
   console.log ( 'Camila tiene ahora ' + result + ' libros')
 }
 excercise9(45)
 
 function excercise10(coins, showcase){
-  const result = coins % showcase
+  const result = remainderOfDivision (coins,showcase);
   console.log ( 'No cabrán ' + result + ' monedas')
 }
 excercise10(123,5)
@@ -66,43 +87,43 @@ excercise10(123,5)
 excercise11(7,100)
 
  function excercise12(chickens, chickenCoop){
-  const result = chickens * chickenCoop
+  const result = multiply (chickens,chickenCoop);
   console.log ( 'Sabrina tiene ' + result + ' gallinas')
 }
 excercise12(25,6)
 
 function excercise13(bullets, gunspace){
-  const result = bullets % gunspace
-  console.log ( 'A Abby le sobrarán ' + result + ' balas')
+  const result = remainderOfDivision(bullets,gunspace);
+  console.log ( 'A Abby le sobrarán '+ result + ' balas')
 }
 excercise13(48,7)
 
 function excercise14(morningRun, afternoonRun){
-  const result = morningRun + afternoonRun
+  const result = sum(morningRun, afternoonRun )
   console.log ( 'Bego corrió en total ' + result + ' km')
 }
 excercise14(3,4)
 
 function excercise15(paper, paperSheet){
-  const result = paper % paperSheet
+  const result = remainderOfDivision (paper, paperSheet);
   console.log ( 'Van a quedar por fuera ' + result + ' hojas')
 }
 excercise15(100,6)
 
 function excercise16(euros, acounts){
-  const result = euros / acounts
+  const result = division (euros, acounts);
   console.log ( 'Cada cuenta recibirá ' + result + ' euros')
 }
 excercise16(30000,4)
 
 function excercise17(zombies, days){
-  const result = zombies / days
+  const result = division (zombies,days);
   console.log ( 'Abby mata ' + result + ' infectados al día')
 }
 excercise17(5201,7)
 
 function excercise18(marbels, marbelBox){
-  const result = marbels * marbelBox
+  const result = multiply (marbels,marbelBox);
   console.log ( 'Bego tiene en total ' + result + ' canicas')
 }
 excercise18(24,3)
@@ -114,55 +135,55 @@ function excercise19(coins,begosCoin){
 excercise19(135,78)
 
 function excercise20(legoPieces,lot){
-  const result= legoPieces % lot
+  const result= remainderOfDivision (legoPieces,lot);
   console.log ('Van a quedar por fuera ' + result + ' piezas de lego')
 }
 excercise20(500,80)
 
 function excercise21(cookies,dogs){
-  const result= cookies / dogs
+  const result= division (cookies,dogs);
   console.log ('Cada perro recibirá ' + result + ' galletas')
 }
 excercise21(72,9)
 
 function excercise22(knives,price){
-  const result= knives * price
+  const result= multiply(knives,price)
   console.log ('Pagará en total ' + result + ' euros')
 }
 excercise22(5,18)
 
 function excercise23(cookies,bag){
-  const result= cookies / bag
+  const result= division (cookies,bag);
   console.log ('Llenará ' + result + ' bolsas de galletas')
 }
 excercise23(480,40)
 
 function excercise24(km,day){
-  const result= km / day
+  const result= division(km,day);
   console.log ('Recorrió ' + result + ' km por día')
 }
 excercise24(1200,4)
 
 function excercise25(january,february){
-  const result= january + february
+  const result= sum(january,february)
   console.log ('Sabrina ahorro ' + result + ' euros')
 }
 excercise25(120,85)
 
 function excercise26(candies,friends){
-  const result= candies % friends
+  const result= remainderOfDivision (candies,friends);
   console.log ('Sobrarán ' + result + ' caramelos')
 }
 excercise26(73,6)
 
 function excercise27(morning,afternoon){
-  const result= morning + afternoon
+  const result= sum(morning, afternoon)
   console.log ('Registró ' + result + ' casas en total')
 }
 excercise27(17,8)
 
 function excercise28(photos,album){
-  const result= photos % album
+  const result= remainderOfDivision (photos,album);
   console.log ('No podrá guardar ' + result + ' fotos')
 }
 excercise28(95,12)
@@ -174,7 +195,7 @@ function excercise29(discount,price){
 excercise29(15,120)
 
 function excercise30(flour,portions){
-  const result= flour % portions
-  console.log ('Sobrarán' + result + ' gramos de harina')
+  const result= remainderOfDivision(flour,portions)
+  console.log ('Sobrarán ' + result + ' gramos de harina')
 }
 excercise30(500,24)
